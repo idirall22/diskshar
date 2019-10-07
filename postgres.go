b03ffe90229f54f7603e6a76928a13180dbd6abf
idir <idirall22@gmail.com>
2019-10-07 21:02:31 +0100

    implemented new func

--- postgres.go
+++ postgres.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 // PostgresProvider postgres implementation of provider interface
@@ -14,7 +15,40 @@ type PostgresProvider struct {
 // New created new user model
 func (p *PostgresProvider) New(ctx context.Context,
 	username, email, firstName, lastName, password, avatar string) (*User, error) {
-	return nil, nil
+
+	query := fmt.Sprintf(`INSERT INTO %s
+		(username, first_name, last_name, email, password, avatar)
+		VALUES ('%s','%s','%s','%s','%s','%s') RETURNING id, created_at`,
+		p.tableName,
+		username,
+		firstName,
+		lastName,
+		email,
+		password,
+		avatar,
+	)
+
+	stmt, err := p.db.Prepare(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{}
+
+	err = stmt.QueryRowContext(ctx).Scan(&user.ID, &user.CreatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+	user.Username = username
+	user.Email = email
+	user.LastName = lastName
+	user.FirstName = firstName
+	user.Password = password
+	user.Avatar = avatar
+
+	return user, nil
 }
 
 // Get get a user model
@@ -31,4 +65,12 @@ func (p *PostgresProvider) Update(ctx context.Context, user *User) error {
 // Delete delete user model
 func (p *PostgresProvider) Delete(ctx context.Context, id int64) error {
 	return nil
+}
+
+func (p *PostgresProvider) getDB() *sql.DB {
+	return p.db
+}
+
+func (p *PostgresProvider) getTableName() string {
+	return p.tableName
 }
