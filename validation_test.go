492fc1b2f8fc21eec7d06ca68ed99d5a8c8424b7
idir <idirall22@gmail.com>
2019-10-08 20:36:59 +0100

    added the tests for validation email and username

---
+++ validation_test.go
@@ -0,0 +1,53 @@
+package user
+
+import (
+	"testing"
+)
+
+// Test email
+func TestValidateEmail(t *testing.T) {
+	emailsTest := []string{
+		"jhon@email.com",
+		"@email.com",
+	}
+
+	for i := 0; i < len(emailsTest); i++ {
+		_, err := validateEmail(emailsTest[i])
+
+		switch i {
+		case 0:
+			if err != nil {
+				t.Error("Error this error should be nil but got :", err)
+			}
+		case 1:
+			if err == nil {
+				t.Error("Error this error should not be nil")
+			}
+		}
+	}
+
+}
+
+// Test email
+func TestValidateUsername(t *testing.T) {
+	usernameTest := []string{
+		"jhonjons12",
+		"@jhon+",
+	}
+
+	for i := 0; i < len(usernameTest); i++ {
+		_, err := validateUsername(usernameTest[i])
+
+		switch i {
+		case 0:
+			if err != nil {
+				t.Error("Error this error should be nil but got :", err)
+			}
+		case 1:
+			if err == nil {
+				t.Error("Error this error should not be nil")
+			}
+		}
+	}
+
+}
