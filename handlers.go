09862f0c195ed6864a743ad0bb473415f482ea25
idir <idirall22@gmail.com>
2019-10-10 12:46:27 +0100

    added login handler

--- handlers.go
+++ handlers.go
@@ -1,9 +1,14 @@
 package user
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 )
+
+// TimeoutRequest timeout a request
+var TimeoutRequest = time.Second * 5
 
 // Register endpoint: "auth/register"
 func (s *Service) Register(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +23,7 @@ func (s *Service) Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	validatedForm, err := validateLoginForm(form)
+	validatedForm, err := validateRegisterForm(form)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusConflict)
 		return
@@ -51,4 +56,32 @@ func (s *Service) Register(w http.ResponseWriter, r *http.Request) {
 // Login endpoint: "auth/login"
 func (s *Service) Login(w http.ResponseWriter, r *http.Request) {
 
+	defer r.Body.Close()
+
+	form := &LoginForm{}
+	err := json.NewDecoder(r.Body).Decode(&form)
+
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	validForm, err := validateLoginForm(form)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusConflict)
+		return
+	}
+
+	ctx, fn := context.WithTimeout(r.Context(), TimeoutRequest)
+	defer fn()
+
+	token, err := s.Authenticate(ctx, validForm.Username, validForm.Email, validForm.Password)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Autherization", "Bearer "+token)
 }
