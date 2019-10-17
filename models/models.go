cf80504169c5ba27ec85eb91c111f2bf2903ca49
idir <idirall22@gmail.com>
2019-10-17 20:34:20 +0100

    moved models to models folder

---
+++ models/models.go
@@ -0,0 +1,36 @@
+package models
+
+import "time"
+
+// RegisterForm model
+type RegisterForm struct {
+	Username string `json:"username"`
+	Email    string `json:"Email"`
+	Password string `json:"password"`
+}
+
+// LoginForm model
+type LoginForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// ValidLoginForm model
+type ValidLoginForm struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// User model
+type User struct {
+	ID        int64      `json:"id"`
+	Username  string     `json:"username"`
+	FirstName string     `json:"first_name"`
+	LastName  string     `json:"last_name"`
+	Email     string     `json:"Email"`
+	Password  string     `json:"password"`
+	Avatar    string     `json:"avatar"`
+	CreatedAt time.Time  `json:"created_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
